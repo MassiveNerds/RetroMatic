2e6a82954cb93c844e88e24694cf5f1c49bae98d
Todd Groff <todd.groff@dudesolutions.com>
2017-11-09 11:00:46 -0500

    changes to env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyCuJFhpKjEYEQ37Wkh51N6fVel9TCAV9Sg',
+    authDomain: 'agile-retrospective-6f3a0.firebaseapp.com',
+    databaseURL: 'https://agile-retrospective-6f3a0.firebaseio.com',
+    projectId: 'agile-retrospective-6f3a0',
+    storageBucket: 'agile-retrospective-6f3a0.appspot.com',
+    messagingSenderId: '466997433892'
   }
 };
