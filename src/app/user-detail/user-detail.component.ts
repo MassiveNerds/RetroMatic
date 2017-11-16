9eed305b5fbb40b05a51cea1e7f04237e1565a4b
Andrew Byrd <byrd286@gmail.com>
2017-11-16 00:36:52 -0500

    Add additional customization options when creating retro

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
-import {BsModalService} from 'ngx-bootstrap/modal';
-import {BsModalRef} from 'ngx-bootstrap/modal/modal-options.class';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 import * as firebase from 'firebase/app';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-user-detail',
@@ -22,8 +23,8 @@ export class UserDetailComponent implements OnInit {
     backdrop: true,
     ignoreBackdropClick: false
   };
-  addbox: any;
-  constructor(private db: AngularFireDatabase, public afAuth: AngularFireAuth, private modalService: BsModalService,) {
+
+  constructor(private db: AngularFireDatabase, public afAuth: AngularFireAuth, private modalService: BsModalService, ) {
     this.user = afAuth.authState;
     this.user.subscribe(result => this.uid = result.uid);
   }
@@ -32,21 +33,17 @@ export class UserDetailComponent implements OnInit {
     this.retroboards = this.db.list(`/retroboards/${this.uid}`)
   }
 
-  openModal(template: TemplateRef<any>, bucket: any, note?: any) {
-    this.addbox = bucket;
-    if (note) {
-      this.addbox = note;
-    }
+  openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, this.config);
   }
 
-  createRetroboard(message: string) {
-    this.retroboards.push({name: message}).then(result => {
+  createRetroboard(name: string, bucket1: string, bucket2: string, bucket3: string, hasTimer: boolean) {
+    this.retroboards.push({ name: (name && name.length > 0) ? name : moment().format('dddd, MMMM Do YYYY'), hasTimer: hasTimer }).then(result => {
       const newId = result.key;
       const buckets: FirebaseListObservable<any[]> = this.db.list(`/buckets/${newId}`);
-      buckets.push({ name: 'What went well?', type: 'success' });
-      buckets.push({ name: 'What can be improved?', type: 'danger' });
-      buckets.push({ name: 'Action items', type: 'info' });
+      buckets.push({ name: (bucket1 && bucket1.length > 0) ? bucket1 : 'What went well?', type: 'success' });
+      buckets.push({ name: (bucket2 && bucket2.length > 0) ? bucket2 : 'What can be improved?', type: 'danger' });
+      buckets.push({ name: (bucket3 && bucket3.length > 0) ? bucket3 : 'Action items', type: 'info' });
     }).then(() => this.modalRef.hide());
   }
 }
