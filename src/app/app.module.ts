ba4275aeab78f60860ef4eb26a761b622c3cfbd6
WordyTheByrd <byrd286@gmail.com>
2018-08-02 00:54:49 -0400

    Added material themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { UIErrorHandler, ModalContentComponent } from './error-handler';
-import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from '../environments/environment';
-import { RetroBoardComponent } from './components/retro-board/retro-board.component';
-import { LoginComponent } from './components/login/login.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { AuthGuard } from './guards/auth.guard';
-import { UserDetailComponent } from './components/user-detail/user-detail.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ExportingComponent } from './components/exporting/exporting.component';
-import { TheHeaderComponent } from './components/the-header/the-header.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ErrorHandler } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { UIErrorHandler, ModalContentComponent } from "./error-handler";
+import { AppComponent } from "./app.component";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { environment } from "../environments/environment";
+import { RetroBoardComponent } from "./components/retro-board/retro-board.component";
+import { LoginComponent } from "./components/login/login.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { UserDetailComponent } from "./components/user-detail/user-detail.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ExportingComponent } from "./components/exporting/exporting.component";
+import { TheHeaderComponent } from "./components/the-header/the-header.component";
 
-import { MatInputModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatListModule } from '@angular/material/list';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from "@angular/material";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatListModule } from "@angular/material/list";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { ThemePickerComponent } from "./components/theme-picker/theme-picker.component";
+import { ThemeStorage } from "./components/theme-picker/theme-storage/theme-storage";
+import { StyleManager } from "./components/style-manager";
 
 const appRoutes: Routes = [
   {
-    path: 'retroboard/:id',
+    path: "retroboard/:id",
     canActivate: [AuthGuard],
-    component: RetroBoardComponent,
+    component: RetroBoardComponent
   },
   {
-    path: 'home',
+    path: "home",
     canActivate: [AuthGuard],
-    component: UserDetailComponent,
+    component: UserDetailComponent
   },
   {
-    path: 'login',
-    component: LoginComponent,
+    path: "login",
+    component: LoginComponent
   },
   {
-    path: '',
-    redirectTo: '/home',
+    path: "",
+    redirectTo: "/home",
     canActivate: [AuthGuard],
-    pathMatch: 'full',
+    pathMatch: "full"
   },
-  { path: '**', component: PageNotFoundComponent },
+  { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
@@ -65,6 +70,7 @@ const appRoutes: Routes = [
     ExportingComponent,
     ModalContentComponent,
     TheHeaderComponent,
+    ThemePickerComponent
   ],
   imports: [
     BrowserModule,
@@ -76,7 +82,7 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: false }, // <-- debugging purposes only
+      { enableTracing: false } // <-- debugging purposes only
     ),
     MatToolbarModule,
     MatButtonModule,
@@ -90,15 +96,18 @@ const appRoutes: Routes = [
     MatMenuModule,
     MatExpansionModule,
     MatBadgeModule,
+    MatGridListModule
   ],
   entryComponents: [ModalContentComponent],
   providers: [
     AuthGuard,
+    StyleManager,
+    ThemeStorage,
     {
       provide: ErrorHandler,
-      useClass: UIErrorHandler,
-    },
+      useClass: UIErrorHandler
+    }
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
