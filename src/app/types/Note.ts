03c7be3535bbd133456ea459ce3bf59ab0f0ee31
andrew.byrd <andrew.byrd@protonmail.com>
2019-12-17 22:47:20 -0500

    add types back

---
+++ src/app/types/Note.ts
@@ -0,0 +1,10 @@
+export class Note {
+  creator: string;
+  creatorId: string;
+  retroboardId: string;
+  bucketId: string;
+  message: string;
+  voteCount: number;
+  votes: { [userId: string]: boolean };
+  key?: string;
+}
